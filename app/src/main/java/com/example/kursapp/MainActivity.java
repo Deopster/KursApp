172275314ab7236c8ea7ec1072604bcc22f20426
Deopster <68296704+Deopster@users.noreply.github.com>
2021-11-10 22:11:05 +0300

    update

--- app/src/main/java/com/example/kursapp/MainActivity.java
+++ app/src/main/java/com/example/kursapp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.kursapp;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -21,10 +24,10 @@ import com.example.kursapp.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     ListView list;
-    Button add;
     EditText add_name;
+    EditText Stud;
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
     Database db;
@@ -33,9 +36,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_gallery);
         list = findViewById(R.id.Students);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        db = new Database(this);
         listitem = new ArrayList<>();
         storeDataInArrays();
         setSupportActionBar(binding.appBarMain.toolbar);
@@ -48,10 +53,12 @@ public class MainActivity extends AppCompatActivity {
                 .setOpenableLayout(drawer)
                 .build();
 
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        setContentView(R.layout.fragment_gallery);
+        Stud = findViewById(R.id.FIO);
+        setContentView(binding.getRoot());
     }
     private void storeDataInArrays(){
         Cursor cursor = db.readAllData();
@@ -76,5 +83,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+    public void addStudent(View v) {
+        Database db = new Database(MainActivity.this);
+        db.addStudent(Stud.getText().toString());
+        Toast.makeText(MainActivity.this,"Student added",Toast.LENGTH_SHORT).show();
+        Stud.setText("");
     }
 }
