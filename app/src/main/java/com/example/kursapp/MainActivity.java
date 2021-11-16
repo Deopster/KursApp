6dc891f96285d6f8137401d4533c004fa61281a8
Deopster <68296704+Deopster@users.noreply.github.com>
2021-11-16 23:31:58 +0300

    test

--- app/src/main/java/com/example/kursapp/MainActivity.java
+++ app/src/main/java/com/example/kursapp/MainActivity.java
@@ -1,48 +1,37 @@
 package com.example.kursapp;
 
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.kursapp.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity{
-    ListView list;
-    EditText add_name;
-    EditText Stud;
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-    Database db;
-    public ArrayList<String> listitem;
-    ArrayAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_gallery);
-        list = findViewById(R.id.Students);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        db = new Database(this);
-        listitem = new ArrayList<>();
-        storeDataInArrays();
+
         setSupportActionBar(binding.appBarMain.toolbar);
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
@@ -52,25 +41,32 @@ public class MainActivity extends AppCompatActivity{
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-        setContentView(R.layout.fragment_gallery);
-        Stud = findViewById(R.id.FIO);
-        setContentView(binding.getRoot());
-    }
-    private void storeDataInArrays(){
-        Cursor cursor = db.readAllData();
-        if(cursor.getCount() != 0){
-            while (cursor.moveToNext()){
-                listitem.add(cursor.getString(0));
+        setContentView(R.layout.activity_main);
+        PreCreateDB.copyDB(this);
+        databaseAdapter = new DatabaseAdapter(this);
+        ListView lvContact = findViewById(R.id.lvContact);
+        final SimpleCursorAdapter simpleCursorAdapter = databaseAdapter.populateListViewFromDB();
+        lvContact.setAdapter(simpleCursorAdapter);
+        lvContact.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = (Cursor) simpleCursorAdapter.getItem(position);
+                String name = cursor.getString(1);
+                String email = cursor.getString(2);
+                Intent intent = new Intent(MainActivity.this, EditContactActivity.class);
+                intent.putExtra("id", id);
+                intent.putExtra("name", name);
+                intent.putExtra("email", email);
+                startActivity(intent);
+                finish();
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
             }
-            adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,listitem);
-            list.setAdapter(adapter);
-        }
-
+        });
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -83,11 +79,5 @@ public class MainActivity extends AppCompatActivity{
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
-    public void addStudent(View v) {
-        Database db = new Database(MainActivity.this);
-        db.addStudent(Stud.getText().toString());
-        Toast.makeText(MainActivity.this,"Student added",Toast.LENGTH_SHORT).show();
-        Stud.setText("");
     }
 }
